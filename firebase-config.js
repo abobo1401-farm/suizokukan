57f71d4c067f20c734cc9d555b423996d1101468
abobo1401-farm <abobo1401@gmail.com>
2026-08-03 20:14:35 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6ZxYvoLJBLLY2NwN5s4qnwbPCiKQmsNs",
+  authDomain: "oekaki-culb.firebaseapp.com",
+  databaseURL: "https://oekaki-culb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "oekaki-culb",
+  storageBucket: "oekaki-culb.firebasestorage.app",
+  messagingSenderId: "1094088069262",
+  appId: "1:1094088069262:web:aeead4c6fe6c05f7b2b9df"
+};
